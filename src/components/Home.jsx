a3c6a2faf176813170519d2f8d07a7a2bf479890
Alex Chen <alexchen@Alexs-MBP.attlocal.net>
2023-01-01 13:53:02 -0500

    Home page filled in

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import {HiArrowNarrowRight} from 'react-icons/hi';
 
 const Home = () => {
   return (
     <div className=' w-full h-screen bg-[#0a192f]'>
+
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-purple-600'>Hi, my name is </p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Alex Chen</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Working my way, to be better every day.</p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-600 hover:border-purple-600'>View Work 
+                <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3'/>
+                </span>
+                </button>
+            </div>
+
+        </div>
     </div>
   )
 }
