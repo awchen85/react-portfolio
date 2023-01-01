287607b3eb0a1e5b4ee230ab6c1f97e7ada93b78
Alex Chen <alexchen@Alexs-MBP.attlocal.net>
2023-01-01 14:04:50 -0500

    setup about section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -7,7 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
-
+      <About />
     </div>
   );
 }
